f6dc3c795bed3cac4ec134ecbaed661e4b0ccd9c
Hussain638 <hussain@techrovar.org>
2025-05-27 19:46:20 +0530

    header text color

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,20 @@
 import { Group, Title } from '@mantine/core';
 import NavigationLinks from './NavigationLinks';
+import { color } from 'framer-motion';
 
 const Header = () => {
   return (
-    <Group justify="space-between" align="center" h="100%" px="md" style={{backgroundColor:"#1c1c1e",color:'#white'}}>
+    <Group justify="space-between" align="center" h="100%" px="md" style={{backgroundColor:"#1c1c1e"}}>
       <Title
         order={3}
-        sx={(theme) => ({
-          fontSize: theme.fontSizes.xl,
-          [theme.fn.smallerThan('sm')]: {
-            fontSize: theme.fontSizes.lg,
-          },
-        })}
+        // sx={(theme) => ({
+        //   fontSize: theme.fontSizes.xl,
+        //   [theme.fn.smallerThan('sm')]: {
+        //     fontSize: theme.fontSizes.lg,
+        //     color:""
+        //   },
+        // })}
+        style={{color:"#fff"}}
       >
         Movie Explorer
       </Title>
